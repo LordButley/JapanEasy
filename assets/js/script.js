2719dce74ae26f002d3eb00810261fe3b445d8a3
LordButley <joshjwwilson@gmail.com>
2021-11-04 23:22:38 +0000

    add JS to increaseScore() and increaseIncorrectScore() to show totals in HTML. Add if loop to determineloop to determine language of question/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const englishCharacters = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo", "n"];
+const englishCharacters = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo", "n"];
 const hiraganaCharacters = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "を", "ん"];
 const numberCharacters = 46;
 let randomQuestionOrder = [];
@@ -7,7 +7,8 @@ let quizDifficulty = 3;
 let questionLanguage;
 let difficultySetting;
 let questionLanguageSetting;
-let question = document.getElementById("question");
+let questionElement = document.getElementById("question");
+let question;
 let answers = [];
 let score = 0;
 let mistakes = 0;
@@ -31,16 +32,23 @@ function quizStart(){
 
 function getQuestion() {
     index = randomQuestionOrder[questionNumber];
-    console.log(index);
-    let answerSet = [hiraganaCharacters[index]];
-    let possibleAnswers = hiraganaCharacters.slice();
+    let answerSet;
+    let possibleAnswers;
+    if (questionLanguage === "English"){
+        question = englishCharacters[index];
+        answerSet = [hiraganaCharacters[index]];
+        possibleAnswers = hiraganaCharacters.slice();
+    }else if(questionLanguage === "Hiragana"){
+        question = hiraganaCharacters[index];
+        answerSet = [englishCharacters[index]];
+        possibleAnswers = englishCharacters.slice();
+    }
     possibleAnswers.splice(index,1);
     possibleAnswers.sort(() => Math.random() - 0.5);
     for (let i = 0; i < (quizDifficulty - 1); i++){
         let newWrongAnswer = possibleAnswers.shift();
         answerSet.push(newWrongAnswer);
     }
-    // questionNumber++;
     answerSet.sort(() => Math.random() - 0.5);
     return answerSet;
 }
@@ -53,13 +61,12 @@ function changeDisplay(){
 function nextQuestion(button){
     checkAnswer(button);
     questionNumber++;
-    console.log(questionNumber);
     answers = getQuestion();
     setTimeout(buildQuiz, 1000);
 }
 
 function buildQuiz(){
-    question.innerHTML = englishCharacters[index] + "?";
+    questionElement.innerHTML = question + "?";
     let answerHTML = ``;
     for(answer of answers){
         let rowHTML = `<button class = "btn" onclick="nextQuestion(this)">${answer}</button>`
@@ -69,24 +76,25 @@ function buildQuiz(){
 }
 
 function checkAnswer (button) {
-    let selected = button.innerHTML;
-    console.log(selected);
-    
+    let selected = button.innerHTML;    
     if(selected == englishCharacters[index] || selected == hiraganaCharacters[index] ){
         increaseScore();
         button.classList.add("correct-answer");
     }else{
-        increaseIncorrectScore;
+        increaseIncorrectScore();
         button.classList.add("incorrect-answer");
     }
 }
 
 function increaseScore (){
     score ++;
+    document.getElementById("correct-counter").innerHTML = score;
 }
 
 function increaseIncorrectScore(){
     mistakes ++;
+    console.log(mistakes);
+    document.getElementById("incorrect-counter").innerHTML = mistakes;
 }
 
 function reset(){
